f6c3cfc23c1a1601bc6fe522e828f6a301bde12b
hoon <ehswkfn123@naver.com>
2025-11-17 08:48:32 +0900

    chore : 폰트 설정

--- src/routes/layouts/Default.tsx
+++ src/routes/layouts/Default.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router";
+import Header from "../../components/Common/Header/Header";
 
 export default function Default() {
   return (
-    <div className='bg-linear-to-r from-(--bg-color-primary) to-(--bg-color-secondary) h-screen'>
-      <header>header</header>
+    <div className='bg-linear-to-r from-(--bg-color-primary) to-(--bg-color-secondary) h-screen font-default'>
+      <Header />
       <Outlet />
       <footer>footer</footer>
     </div>
