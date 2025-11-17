c9df588b7a1c794dde26844073aad05642cd55fc
hoon <ehswkfn123@naver.com>
2025-11-18 00:31:20 +0900

    design : 난이도 선택 페이지 기본 레이아웃 설정

--- src/components/Common/ui/Button.tsx
+++ src/components/Common/ui/Button.tsx
@@ -2,12 +2,11 @@ import { Link } from "react-router";
 
 export default function Button({ text, linkTo }: ButtonProps) {
   return (
-    <>
-      <Link to={linkTo}>
-        <button className='bg-green-400 text-3xl  px-36 py-3 cursor-pointer rounded-md'>
-          {text}
-        </button>
-      </Link>
-    </>
+    <Link
+      to={linkTo}
+      className='bg-green-400 text-3xl px-36 py-3 cursor-pointer rounded-md text-center'
+    >
+      {text}
+    </Link>
   );
 }
--- src/routes/pages/SelectDifficulty/SelectDifficulty.tsx
+++ src/routes/pages/SelectDifficulty/SelectDifficulty.tsx
@@ -1,7 +1,13 @@
+import Button from "../../../components/Common/ui/Button";
+
 export default function SelectDifficulty() {
   return (
     <>
-      <h1>SelectDifficulty Component</h1>
+      <div className='flex flex-col gap-30 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <Button text='쉬움' linkTo='/selectDifficulty' />
+        <Button text='보통' linkTo='/selectDifficulty' />
+        <Button text='어려움' linkTo='/selectDifficulty' />
+      </div>
     </>
   );
 }
