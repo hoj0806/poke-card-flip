6378cf176a11a8ee7ef304e67e7e54f58dfd1017
hoon <ehswkfn123@naver.com>
2025-11-17 13:27:36 +0900

    design : footer 기본 디자인

---
+++ src/components/Common/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className='bg-black mt-auto text-right py-1.5 px-5'>
+        <a
+          href='https://github.com/hoj0806/poke-card-flip'
+          className='text-white'
+        >
+          git repository
+        </a>
+      </footer>
+    </>
+  );
+}
--- src/routes/layouts/Default.tsx
+++ src/routes/layouts/Default.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "../../components/Common/Header/Header";
+import Footer from "../../components/Common/Footer/Footer";
 
 export default function Default() {
   return (
     <div className='bg-linear-to-r from-(--bg-color-primary) to-(--bg-color-secondary) font-default flex flex-col h-screen'>
       <Header />
       <Outlet />
-      <footer>footer</footer>
+      <Footer />
     </div>
   );
 }
