b68eb405134a7d3ee098512cc689e26fc113beca
hoon <ehswkfn123@naver.com>
2025-11-20 06:44:21 +0900

    design : 게임 오버 팝업 디자인

--- src/routes/layouts/GameDefault.tsx
+++ src/routes/layouts/GameDefault.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router";
-import ProgressBarTimer from "../../components/Game/ProgressBarTimer";
 
 export default function GameDefault() {
   return (
     <>
       <h1>GameDefault Component</h1>
-      <ProgressBarTimer />
       <Outlet />
     </>
   );
