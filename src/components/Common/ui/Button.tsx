a359684f60eaf99de8b178c4ac0ed716c8ba5b08
hoon <ehswkfn123@naver.com>
2025-11-21 14:14:43 +0900

    ui : button 컴포넌트 디자인 수정

--- src/components/Common/ui/Button.tsx
+++ src/components/Common/ui/Button.tsx
@@ -1,10 +1,31 @@
 import { Link } from "react-router";
 
-export default function Button({ text, linkTo }: ButtonProps) {
+export default function Button({ text, linkTo, color = "sky" }: ButtonProps) {
+  // Tailwind 색상 맵
+  const colorMap: Record<ButtonColor, string> = {
+    sky: "bg-sky-300 hover:bg-sky-400",
+    green: "bg-green-400 hover:bg-green-500",
+    red: "bg-red-400 hover:bg-red-500",
+    yellow: "bg-yellow-400 hover:bg-yellow-500",
+  };
+
   return (
     <Link
       to={linkTo}
-      className='bg-green-400 text-3xl px-36 py-3 cursor-pointer rounded-md text-center'
+      className={`
+        inline-block
+        px-25
+        py-4
+        text-3xl  
+        text-white
+        border-4 border-black
+        shadow-[4px_4px_0_0_black]
+        active:shadow-none active:translate-x-1 active:translate-y-1
+        rounded-md
+        text-center
+        ${colorMap[color]}
+        hover:text-black
+      `}
     >
       {text}
     </Link>
--- src/routes/pages/SelectDifficulty/SelectDifficulty.tsx
+++ src/routes/pages/SelectDifficulty/SelectDifficulty.tsx
@@ -3,11 +3,11 @@ import Button from "../../../components/Common/ui/Button";
 export default function SelectDifficulty() {
   return (
     <>
-      <div className='flex flex-col gap-30 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-        <Button text='쉬움' linkTo='/game/easy' />
-        <Button text='보통' linkTo='/game/normal' />
-        <Button text='어려움' linkTo='/game/hard' />
-        <Button text='메인으로' linkTo='/' />
+      <div className='flex flex-col gap-16 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <Button text='쉬움' linkTo='/game/easy' color='red' />
+        <Button text='보통' linkTo='/game/normal' color='yellow' />
+        <Button text='어려움' linkTo='/game/hard' color='green' />
+        <Button text='메인으로' linkTo='/' color='sky' />
       </div>
     </>
   );
